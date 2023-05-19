3e8ef652fec01ad00d27a1ec130d52ecda1c8dc5
Victor Blanco <12vblanco@gmail.com>
2023-05-19 23:58:36 +0300

    scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { styled } from "styled-components";
+import logo from "./assets/logo.png";
 import img1 from "./assets/menu1.png";
 import img2 from "./assets/menu2.png";
 
 const App = () => {
   const [backgroundColor, setBackgroundColor] = useState("");
+  const [showSecondImage, setShowSecondImage] = useState(false);
+  const secondImageRef = useRef(null);
 
   useEffect(() => {
     const currentDate = new Date();
@@ -31,12 +34,34 @@ const App = () => {
     setBackgroundColor(selectedColor);
   }, []);
 
+  useEffect(() => {
+    const handleTouchMove = () => {
+      if (secondImageRef.current) {
+        const { top, bottom } = secondImageRef.current.getBoundingClientRect();
+        setShowSecondImage(top <= window.innerHeight && bottom >= 0);
+      }
+    };
+
+    window.addEventListener("touchmove", handleTouchMove);
+    return () => window.removeEventListener("touchmove", handleTouchMove);
+  }, []);
+
   return (
     <Container style={{ background: backgroundColor }}>
       <Div>
-        <Img src={img1} alt="menu 1" />
-        <Img src={img2} alt="menu 2" />
-      </Div>{" "}
+        <Image>
+          <Logo src={logo} alt="El logo del Bar Rio" />
+          <Img src={img1} alt="menu 1" />
+        </Image>
+        <Image>
+          <Img
+            ref={secondImageRef}
+            src={img2}
+            alt="menu 2"
+            className={showSecondImage ? "show" : ""}
+          />
+        </Image>
+      </Div>
     </Container>
   );
 };
@@ -46,11 +71,11 @@ const Container = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  height: fit-content;
   width: 100vw;
-  height: 100vh;
+  min-height: 100vh;
   @media (max-width: 770px) {
     height: fit-content;
+    width: 100%;
   }
 `;
 
@@ -60,12 +85,15 @@ const Div = styled.div`
   width: 100%;
   align-items: center;
   justify-content: center;
+`;
 
-  @media (max-width: 770px) {
+const Image = styled.div`
+  @media (max-width: 400px) {
+    display: flex;
+    justify-content: center;
+    align-items: center;
     width: 100%;
-    height: 100%;
-    min-width: 340px;
-    margin-top: 22px;
+    height: 100vh;
   }
 `;
 
@@ -80,6 +108,26 @@ const Img = styled.img`
     width: 100%;
     height: auto;
     min-width: 340px;
+    margin: 0;
+    padding: 22px;
+  }
+  &.show {
+    height: auto;
+  }
+`;
+
+const Logo = styled.img`
+  width: 88px;
+  position: absolute;
+  top: 2%;
+  left: 80%;
+  transform: rotate(25deg);
+  @media (max-width: 400px) {
+    width: 88px;
+    position: absolute;
+    top: 30px;
+    left: 230px;
+    transform: rotate(25deg);
   }
 `;
 
